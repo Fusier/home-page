723562566dc1679bb323d5336dc84bf6947aaa63
Fusier <samukoulu@gmail.com>
2021-01-15 02:33:28 +0200

    Remove strict mode from render

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './pages/App';
-import reportWebVitals from './reportWebVitals';
+import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-reportWebVitals();
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+//<React.StrictMode>
+//</React.StrictMode>
